9985ee884404dc96bce4f23c4228daa1453cd6c5
Mehran Roham <mehranroham@gmail.com>
2024-04-17 04:45:45 +0330

    working with objects basic

---
+++ assets/scripts/objects.js
@@ -0,0 +1,28 @@
+// const person = {
+//   'first name': 'Mehran',
+//   age: 26,
+//   hobbies: ['Movies', 'Coding'],
+//   greet() {
+//     console.log('Hi there!');
+//   },
+// };
+
+// // person.greet();
+
+// person.age = 31;
+
+// person.isAdmin = true;
+
+// delete person.age;
+
+// console.log(person['first name']);
+
+// person[4030] = 'Chelsea';
+
+// console.log(person[4030]);
+
+// console.dir(person);
+
+// function me() {
+//   console.log('hello');
+// }
